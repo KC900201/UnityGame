90ba7ebe52db52535363e7ad5a5a026c27c34e65
kc900201 <nkc900201@gmail.com>
2020-07-28 16:11:42 +0900

    Update source code on leanrning collision, gameplay settings, c sharp scripts on controlling player movements - 07282020

---
+++ KC_Unity/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Script to enable camera follow
+ */
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset; // store 3 floats, vector of an object (x, y z)
+
+    /* Not needed
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    */
+    /*
+     * Purpose:to enable camera snap to position of player object each frame
+     */
+    // Update is called once per frame
+    void Update()
+    {
+//        Debug.Log(player.position);
+        // transform -> transform of current object (camera)
+        transform.position = player.position + offset; // Feed the position of the player object to camera
+    }
+}
---
+++ KC_Unity/Assets/Scripts/HitCollision.cs
@@ -0,0 +1,19 @@
+﻿/*This script is to enable hit collision during player movement*/
+//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCollision : MonoBehaviour
+{
+    public PlayerMovement movement; // a reference to our player movement script
+
+    void OnCollisionEnter (Collision collisionInfo) // gather information about collision
+    {   // check using tags instead of name for easy checking a group objects under 1 tag, name is individual
+        if (collisionInfo.collider.tag == ("Obstacle")) 
+        {
+            Debug.Log("Hit " + collisionInfo.collider.name);
+            movement.enabled = false; // disable our player movement script if object is hit
+        }
+    }
+
+}
