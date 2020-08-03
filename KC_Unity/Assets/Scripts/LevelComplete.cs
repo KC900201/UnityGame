40391d34b07a899607daf10e78c400d5facad7b4
kc900201 <nkc900201@gmail.com>
2020-08-03 12:06:23 +0900

    Update with Finalized content - 08032020

---
+++ KC_Unity/Assets/Scripts/LevelComplete.cs
@@ -0,0 +1,16 @@
+﻿/* 
+ * This script is enable next level loading 
+ * whenever current level is completed successfully
+ */
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelComplete : MonoBehaviour
+{
+  public void LoadNextLevel()
+   {
+        // Load next level by referring to build index of next scene
+//        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
