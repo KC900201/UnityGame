3fc3a1479a59a02d14db995c151b3af4554d547e
kc900201 <nkc900201@gmail.com>
2020-08-14 08:46:27 +0900

    Build HighScore Board and random number generation - 08142020

---
+++ KC_Unity/Assets/Scripts/Dice.cs
@@ -0,0 +1,33 @@
+﻿/* This is to configure dice enginer */
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dice : MonoBehaviour
+{
+    public Text score;
+    public Text highScore;
+
+    void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+
+    public void RollDice ()
+    {
+        int diceScore = Random.Range(1, 99);
+        
+        score.text = diceScore.ToString();
+
+        // update high score if random score is higher than current high score
+        if (diceScore > PlayerPrefs.GetInt("HighScore", 0)) {
+            PlayerPrefs.SetInt("HighScore", diceScore);
+            highScore.text = diceScore.ToString();
+        } 
+    }
+
+    public void Reset ()
+    {
+        PlayerPrefs.DeleteAll(); // delete all preferences to reset
+        highScore.text = "0";
+    }
+}
