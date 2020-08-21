8e8730f648bad000604b7b3c3f883631ab5fef0c
kc900201 <nkc900201@gmail.com>
2020-08-21 18:52:06 +0900

    08212020

---
+++ KC_Unity/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * This is a sample script to create a power up!
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+
+    public GameObject pickupEffect;
+    public float multiplier = 2.4f;
+
+
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Pickup(other);
+        }
+    }
+
+    void Pickup (Collider player)
+    {
+        Debug.Log("Power up picked up!");
+        // Spawn a cool effect
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        // Apply effect to the player
+        player.transform.localScale *= multiplier;
+
+        // Remove power up object
+        Destroy(gameObject);
+    }
+}
