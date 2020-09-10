b1e0437b37325e7d4fb42aca0320800bcf759284
kc900201 <nkc900201@gmail.com>
2020-09-10 18:14:00 +0900

    Add in project for learning NavMesh. Tutorial on sculpting, smoothing material using ProBuilder and Polybrush - 09102020

---
+++ NavMesh_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * This Script is used to control player (mesh agent) 
+ * movement during navigation in mesh surface
+ */
+using UnityEngine;
+using UnityEngine.AI; // library for controlling agents
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Move our agent
+                agent.SetDestination(hit.point);
+            }
+        }
+        
+    }
+}
